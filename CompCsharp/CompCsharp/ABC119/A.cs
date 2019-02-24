eee33790cb4c0e82391483e56c457c556b93e5c1
mmatthew <takeknock@gmail.com>
2019-02-24 22:55:19 +0900

    initial commit

---
+++ CompCsharp/CompCsharp/ABC119/A.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompCsharp.ABC119
+{
+    class A
+    {
+        //static void Main(string[] args)
+        //{
+        //    string[] s = Console.ReadLine().Split('/');
+        //    int year = int.Parse(s[0]);
+        //    int month = int.Parse(s[1]);
+        //    int day = int.Parse(s[2]);
+
+        //    if (IsHeisei(year, month, day))
+        //    {
+        //        Console.WriteLine("Heisei");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("TBD");
+        //    }
+
+        //}
+
+        static bool IsHeisei(int year, int month, int day)
+        {
+            if (year >= 2020)
+            {
+                return false;
+            }
+
+            if (year == 2019 && month >= 5)
+            {
+                return false;
+            }
+
+            return true;
+
+        }
+
+    }
+}
---
+++ CompCsharp/CompCsharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompCsharp
+{
+    class Program
+    {
+        private const double BTCJPY = 380000.0;
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<double> ret = new List<double>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                double x = double.Parse(s[0]);
+                if (s[1] == "BTC")
+                {
+                    x *= BTCJPY;
+                }
+                ret.Add(x);
+            }
+
+            Console.WriteLine(ret.Sum());
+                
+        }
+    }
+}
