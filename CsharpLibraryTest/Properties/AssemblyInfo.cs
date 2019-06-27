11f8a9dca91fc58354c86dccb4a54affd0a7dc04
mmatthew <takeknock@gmail.com>
2019-06-27 18:36:16 +0900

    test: add Swap test

---
+++ CsharpLibraryTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CsharpLibraryTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CsharpLibraryTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f46a85c-1c4b-4fb0-9374-c3d519db2c59")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CsharpLibraryTest/UtilTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CsharpLibrary;
+
+namespace CsharpLibraryTest
+{
+    [TestClass]
+    public class UtilTest
+    {
+        [TestMethod]
+        public void TestSwap()
+        {
+            int x = 1;
+            int y = 2;
+
+            Util.Swap(ref x, ref y);
+            Assert.AreEqual("2 1", string.Format("{0} {1}", x, y));
+        }
+    }
+}
